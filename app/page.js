20bcab35901c5860a3e3e5a75b3b61f8ffcdd71e
EmreSahna <emre.sahna@hotmail.com>
2023-09-14 22:45:57 +0300

    little fixes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import {getAnasayfaUstKisim} from "@/lib/api";
 import UcluRow from "@/components/uclu-row";
 import IkiliSlider from "@/components/ikili-slider";
 
-export default async function Home() {
+export default async function Page() {
     const posts = await getAnasayfaUstKisim(30)
     const ucluRow = posts.slice(0, 3)
     const ikiliSlayt = posts.slice(3, 11)
