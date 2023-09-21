cecfe7de004a93c1262b9097b3ff8b5db478f545
EmreSahna <emre.sahna@hotmail.com>
2023-09-21 23:43:18 +0300

    design improvements

--- app/kategori/[slug]/page.js
+++ app/kategori/[slug]/page.js
@@ -4,13 +4,20 @@ export default async function Category({params}) {
     const { slug } = params
     const res = await getPostsByCategoryName(slug)
 
-    return (
-        <div className="mx-auto max-w-[1180px] my-6 flex">
+    const chunkSize = 3;
+    const groupedPosts = [];
+
+    for (let i = 0; i < res.length; i += chunkSize) {
+        groupedPosts.push(res.slice(i, i + chunkSize));
+    }
+
+    const renderedComponents = groupedPosts.map((chunk, index) => (
+        <div className="h-[250px] flex mt-4 font-semibold w-full">
             {
-                res.map((post,index) => {
+                chunk.map((post, index) => {
                     post = post.attributes
                     return (
-                        <div className="bg-yblue h-[250px] w-1/2 mx-4" key={index}>
+                        <div className="w-1/3 h-full px-2" key={index}>
                             <a href={"/haber/"+post.Slug} className="relative">
                                 <img src={process.env.IMG_URL + post.Banner.data.attributes.url} alt={post.Banner.data.attributes.alternativeText} className="object-cover w-full h-full" />
                                 <div className="absolute bottom-0 bg-yblue w-full p-2">
@@ -20,6 +27,14 @@ export default async function Category({params}) {
                         </div>
                     )
                 })
+            }
+        </div>
+    ));
+
+    return (
+        <div className="mx-auto max-w-[1180px] my-6 flex flex-wrap">
+            {
+                renderedComponents
             }
         </div>
     )
