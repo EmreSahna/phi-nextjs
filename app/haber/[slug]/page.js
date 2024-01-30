f47e2b05d9167accba978b9c2c5a45ef2400a27b
İsmail Ertuğlu <ismailertgl@gmail.com>
2024-01-31 02:21:30 +0300

    Category and News Detail UI refactor

--- app/haber/[slug]/page.js
+++ app/haber/[slug]/page.js
@@ -24,12 +24,44 @@ export default async function Page({ params }) {
     const { slug } = params
 
     const res = await getSinglePost(slug)
+    console.log(res.Content)
     console.log(dateToTR(res.createdAt));
     return(
-        <div className="max-w-[1180px] mx-auto article">
-            <h1>{res.Title}</h1>
-            <div className="mb-6">
-                <article dangerouslySetInnerHTML={{__html: res.Content}}></article>
+        <div className="max-w-[1180px] mx-auto ">
+            <h2 className="font-bold text-4xl xl:mx-0 mx-4 my-2">{res.Title}</h2>
+            <div className="article xl:mx-0 mx-4">
+                <article
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        <style>
+                            @media (max-width: 1200px) {
+                                img {
+                                    padding-left: 12px;
+                                    padding-right: 12px;
+                                }
+                            }
+                            h1 {
+                                font-size: 44px!important;
+                                font-weight: 700!important;
+                                padding-top: 40px;
+                            }
+                            h4 {
+                                font-size: 20px!important;
+                                font-weight: 500!important;
+                                padding-top: 20px;
+                            }
+                            article {
+                                max-width: 850px!important;
+                            }
+                            p {
+                                font-size: 20px!important;
+                                margin-top: 35px!important;
+                            }
+                        </style>
+                        ${res.Content}
+                        `,
+                    }}
+                />
             </div>
         </div>
     )
