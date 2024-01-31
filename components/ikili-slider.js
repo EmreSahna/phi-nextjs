72547d282de8fa480de9281578548b47e555d7e1
ismailertuglu14 <ismailertgl@gmail.com>
2024-01-31 23:14:06 +0300

    Swiper Title background width parameter fix.

--- components/ikili-slider.js
+++ components/ikili-slider.js
@@ -1,7 +1,7 @@
 'use client'
 
-import {Swiper, SwiperSlide} from "swiper/react";
-import {Autoplay, EffectFade, Pagination} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
 
 import 'swiper/css';
 import 'swiper/css/effect-fade';
@@ -11,7 +11,7 @@ import toImageUrl from "@/utils/to_image_url";
 
 export default function IkiliSlider({ news }) {
     const sliderItems = news.slice(0, 6)
-    const otherItems = news.slice(6,8)
+    const otherItems = news.slice(6, 8)
 
     const pagination = {
         clickable: true,
@@ -21,20 +21,20 @@ export default function IkiliSlider({ news }) {
     };
 
     return (
-        <div className="sm:h-[420px] flex flex-wrap sm:flex-nowrap mt-2 sm:mt-4 font-semibold">
+        <div className="sm:h-[420px] flex flex-wrap sm:flex-nowrap mt-2 sm:mt-8 font-semibold">
             <div className="h-[250px] sm:h-full w-full sm:w-2/3 px-2 flex">
-                <Swiper pagination={pagination} modules={[Autoplay, Pagination, EffectFade]} autoplay={{delay: 3000, disableOnInteraction: false}} loop={true} effect={'fade'}>
+                <Swiper pagination={pagination} modules={[Autoplay, Pagination, EffectFade]} autoplay={{ delay: 3000, disableOnInteraction: false }} loop={true} effect={'fade'}>
                     {
-                        sliderItems.map((slide,index) => {
+                        sliderItems.map((slide, index) => {
                             slide = slide.attributes
                             return (
                                 <SwiperSlide key={index}>
-                                    <a href={"/haber/"+slide.Slug} className="relative">
+                                    <a href={"/haber/" + slide.Slug} className="relative">
                                         <div className="absolute top-0 ml-1 mt-1 bg-yblue">
                                             <span className="text-[#fff] m-1">{slide.categories.data[0].attributes.Name}</span>
                                         </div>
                                         <img src={toImageUrl(slide.Banner.data.attributes.url)} alt={slide.Banner.data.attributes.alternativeText} className="h-full w-full object-cover before:grad" />
-                                        <div className="absolute bottom-12 bg-ychar text-center p-1">
+                                        <div className="absolute bottom-12 bg-ychar text-center p-1 w-full">
                                             <span className="text-[#fff] text-lg sm:text-3xl">{slide.Title}</span>
                                         </div>
                                     </a>
@@ -50,12 +50,12 @@ export default function IkiliSlider({ news }) {
                         other = other.attributes
                         return (
                             <div className="h-[250px] sm:h-1/2 px-2 pb-1 mt-2 sm:mt-0" key={index}>
-                                <a href={"/haber/"+other.Slug}
-                                   className="relative">
+                                <a href={"/haber/" + other.Slug}
+                                    className="relative">
                                     <div className="absolute top-0 ml-1 mt-1 bg-yblue">
                                         <span className="text-[#fff] m-1">{other.categories.data[0].attributes.Name}</span>
                                     </div>
-                                    <img src={toImageUrl(other.Banner.data.attributes.url)} alt={other.Banner.data.attributes.alternativeText} className="h-full w-full object-cover"/>
+                                    <img src={toImageUrl(other.Banner.data.attributes.url)} alt={other.Banner.data.attributes.alternativeText} className="h-full w-full object-cover" />
                                     <div className="absolute bottom-0 bg-yblue w-full p-2">
                                         <span className="text-[#fff] text-lg">{other.Title}</span>
                                     </div>
